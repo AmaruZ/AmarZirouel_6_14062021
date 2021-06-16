82c9f0d61a2ccbee5fddf9d202638a3c1323f71d
Amar <a.zirouel@gmail.com>
2021-06-16 11:50:52 +0200

    ajout de nav html

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,8 +1,16 @@
-fetch("./FishEyeData.json")
+
+let photographers= {};
+
+
+
+fetch("./scripts/FishEyeData.json")
     .then(function(resp){
         return resp.json();
     })
     .then(function(data){
-        console.log(data);
+        for (let photographer in data.photographers){
+            photographers[photographer] = data.photographers[photographer];
+            console.log(photographers[photographer])
+        }
     });
     
