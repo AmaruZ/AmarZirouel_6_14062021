f3b36fb6e12e3cd7043427db29ec26d6044a2cb6
Amar <a.zirouel@gmail.com>
2021-06-21 11:58:19 +0200

    ajout des photographes dans le DOM

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,5 @@
-
-let photographers= {};
+const wrapperPhotographers = document.querySelector(".wrapper");
+let photographers= [];
 
 
 
@@ -10,7 +10,42 @@ fetch("./scripts/FishEyeData.json")
     .then(function(data){
         for (let photographer in data.photographers){
             photographers[photographer] = data.photographers[photographer];
-            console.log(photographers[photographer])
+            addPhotographerInDOM(photographers[photographer]);
+
+
+            //console.log(photographers[photographer]);
         }
     });
-    
+
+
+const addDescription = (photographers) =>{
+    if (photographers.description != ""){
+        switch(photographers.name){
+            case "Mimi Keel": photographers.description = "Voir le beau dans le quotidien";
+            break;
+        }
+    }
+}
+
+const addPhotographerInDOM = (photographer) =>{
+    const divPhotographer = document.createElement("div");
+    divPhotographer.classList.add("photographer")
+    wrapperPhotographers.appendChild(divPhotographer);
+    divPhotographer.innerHTML=  "<a class=\"photographer__link\" href=\"#\">"+
+                                "<img class=\"photographer__avatar\" src=\"./images/Photographers ID Photos/"+ photographer.name.replaceAll(/[^a-zA-Z0-9]/g,"") +".jpg\">"+
+                                "<h2 class=\"photographer__name\">" + photographer.name + "</h2></a>"+
+                                "<p class=\"photographer__text\"><span class=\"photographer__localisation\">" + photographer.city + ", " + photographer.country + "</span>"+
+                                "<span class=\"photographer__tagline\">" + photographer.tagline + "</span>"+
+                                "<span class=\"photographer__price\">" + photographer.price + "</span></p>"+
+                                "<ul class=\"photographer__tags\"> " + tagInList(photographer.tags) + "</ul>";
+                                
+    console.log(divPhotographer);
+}
+
+const tagInList = (tags) =>{
+    let result = "";
+    tags.forEach(tag => {
+        result += "<li>#"+ tag.charAt(0).toUpperCase() + tag.slice(1) + "</li>";
+    });
+    return result;
+}
