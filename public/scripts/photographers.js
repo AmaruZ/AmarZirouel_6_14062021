0d4c75d91b78cc781fc77543a6be8e24678a0dfe
Amar <a.zirouel@gmail.com>
2021-07-03 16:10:10 +0200

    ajout de la galerie medias dans le DOM

--- public/scripts/photographers.js
+++ public/scripts/photographers.js
@@ -5,14 +5,19 @@ import { mediaFactory } from "./media.js";
 
 const urlParams = new URLSearchParams(window.location.search);
 const photographerID = urlParams.get("id");
-export let photographer = {};
-let medias = [];
+
 const wrapperPhotographer = document.querySelector(".infos");
 const namePhotographer = document.querySelector(".infos__name");
 const localisationPhotographer = document.querySelector(".infos__localisation");
 const taglinePhotographer = document.querySelector(".infos__tagline");
 const tagsPhotographer = document.querySelector(".infos__tags");
 const avatarPhotographer = document.createElement("img");
+
+const wrapperMedias = document.querySelector(".medias__wrapper");
+
+export let photographer = {};
+let medias = [];
+
 
 fetchPhotographersJSON()
     .then(data =>{
@@ -50,4 +55,34 @@ fetchPhotographersJSON()
         avatarPhotographer.classList.add("photographer__avatar");
         avatarPhotographer.classList.add("infos__avatar");
         wrapperPhotographer.appendChild(avatarPhotographer);
+        medias.forEach(media => addMediasInDOM(media));
     }) 
+
+const addMediasInDOM = (media) =>{
+    const mediaCard = document.createElement("div");
+    mediaCard.classList.add("media__card");
+    wrapperMedias.appendChild(mediaCard);
+    if(media.image!= undefined){
+        mediaCard.innerHTML =   "<img class=\"media__photo\" src=\"../images/" + getFolderName(photographer) + media.image + "\"/>"
+                            + "<div class=\"media__text\"><span class=\"media__title\">"+ media.title + "</span>"
+                            + "<span class=\"media__likes\">"+media.likes + "</span></div>";
+    } else if(media.video != undefined){
+        mediaCard.innerHTML =  `<video src="../images/${getFolderName(photographer)}${media.video}#t=0.1" type="video/mp4" class="media__photo">Sorry, your browser doesn't support embedded videos.</video>`
+                            + "<div class=\"media__text\"><span class=\"media__title\">"+ media.title + "</span>"
+                            + "<span class=\"media__likes\">"+media.likes + "</span></div>";
+    }
+    
+    console.log(mediaCard);
+    
+}
+
+const getFolderName = (photographer) =>{
+    switch(photographer.name){
+        case "Mimi Keel": return "Mimi/";
+        case "Ellie-Rose Wilkens": return "Ellie Rose/";
+        case "Tracy Galindo": return "Tracy/";
+        case "Nabeel Bradford" : return "Nabeel/";
+        case "Rhode Dubois" : return "Rhode/";
+        case "Marcel Nikolic" : return "Marcel/";
+    }
+}
