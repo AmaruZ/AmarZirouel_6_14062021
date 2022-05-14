d90bd59b93cf4c27aecbe5d2cfda1be1fcd4043a
Amar <a.zirouel@gmail.com>
2022-05-14 13:24:45 +0200

    Restruct folders

---
+++ scripts/app.js
@@ -0,0 +1,125 @@
+import { fetchPhotographersJSON } from './getData.js'
+import { setAvatarToPhotographer } from './setAvatarToPhotographer.js'
+
+const wrapperPhotographers = document.querySelector('.wrapper')
+const spanTags = document.getElementsByClassName('navigation__link')
+export let photographers = []
+
+fetchPhotographersJSON('../data/FishEyeData.json')
+    .then((data) => {
+        data.photographers.forEach((photographer) => {
+            photographers.push(photographer)
+            addPhotographerInDOM(photographer)
+        })
+    })
+    .then(() => {
+        Array.from(spanTags, (tag) => {
+            tag.addEventListener('click', () => {
+                filterByTag(tag)
+            })
+            tag.addEventListener('keypress', (e) => {
+                if (e.key === 'Enter') {
+                    filterByTag(tag)
+                }
+            })
+        })
+        window.addEventListener('scroll', () => {
+            let anchor = document.querySelector('.anchor')
+            let y = window.scrollY
+            if (y >= 160) {
+                anchor.style.display = 'block'
+            } else {
+                anchor.style.display = 'none'
+            }
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
+const addPhotographerInDOM = (photographer) => {
+    const divPhotographer = document.createElement('article')
+    divPhotographer.classList.add('photographer')
+    wrapperPhotographers.appendChild(divPhotographer)
+    divPhotographer.innerHTML = `<a class="photographer__link" href="./pages/photographers.html?id=${
+        photographer.id
+    }">
+                                <img class="photographer__avatar" src="assets/${setAvatarToPhotographer(
+                                    photographer.name
+                                )}" alt="${photographer.name}">
+                                <h2 class="photographer__name">${photographer.name}</h2></a>
+                                <p class="photographer__text" tabindex="0"><span class="photographer__localisation">${
+                                    photographer.city
+                                }, ${photographer.country}</span>
+                                <span class="photographer__tagline">${photographer.tagline}</span>
+                                <span class="photographer__price">${
+                                    photographer.price
+                                }€/jour</span></p>
+                                <div class="photographer__tags">${tagInList(
+                                    photographer.tags
+                                )}</div>`
+}
+
+const filterByTag = (tag) => {
+    flushPhotographersInDOM()
+    console.log(tag)
+    if (tag.classList.contains('tags-active')) {
+        tag.classList.toggle('tags-active')
+        tag.setAttribute('aria-label', `tag ${tag.textContent.slice(1)} désélectionné`)
+        tag.setAttribute('aria-label', `tag ${tag.textContent.slice(1)}`)
+        tag.removeAttribute('aria-selected')
+        tag.blur()
+        photographers.forEach((photographer) => {
+            addPhotographerInDOM(photographer)
+        })
+    } else {
+        for (let i = spanTags.length; i > 0; i--) {
+            if (spanTags[i - 1].classList.contains('tags-active')) {
+                spanTags[i - 1].classList.toggle('tags-active')
+                spanTags[i - 1].setAttribute(
+                    'aria-label',
+                    `tag ${spanTags[i - 1].textContent.slice(1)}`
+                )
+                spanTags[i - 1].removeAttribute('aria-selected')
+                break
+            }
+        }
+
+        tag.classList.toggle('tags-active')
+        tag.setAttribute('aria-label', `tag ${tag.textContent.slice(1)} sélectionné`)
+        tag.setAttribute('aria-selected', 'true')
+        for (let i = 0; i < photographers.length; i++) {
+            console.log(photographers)
+            for (let j = photographers[i].tags.length; j > 0; j--) {
+                console.log(photographers[i])
+                if (
+                    '#' +
+                        photographers[i].tags[j - 1].charAt(0).toUpperCase() +
+                        photographers[i].tags[j - 1].slice(1) ===
+                    tag.textContent
+                ) {
+                    addPhotographerInDOM(photographers[i])
+                    break
+                } else {
+                    console.log('la')
+                }
+            }
+        }
+    }
+}
+
+const flushPhotographersInDOM = () => {
+    while (wrapperPhotographers.firstChild) {
+        wrapperPhotographers.firstChild.remove()
+    }
+}
+
+const tagInList = (tags) => {
+    let result = ''
+    tags.forEach((tag) => {
+        result += `<span class="tags" tabindex="0" aria-label="tag">#${tag
+            .charAt(0)
+            .toUpperCase()}${tag.slice(1)} </span>`
+    })
+    return result
+}
---
+++ scripts/setAvatarToPhotographer.js
@@ -0,0 +1,16 @@
+export function setAvatarToPhotographer(name) {
+    switch (name) {
+        case 'Mimi Keel':
+            return 'Mimi/Portrait_Nora.jpg'
+        case 'Ellie-Rose Wilkens':
+            return 'Ellie Rose/Architecture_Horseshoe.jpg'
+        case 'Tracy Galindo':
+            return 'Tracy/Fashion_Urban_Jungle.jpg'
+        case 'Nabeel Bradford':
+            return 'Nabeel/Travel_Outdoor_Baths.jpg'
+        case 'Rhode Dubois':
+            return 'Rhode/Fashion_Melody_Red_on_Stripes.jpg'
+        case 'Marcel Nikolic':
+            return 'Marcel/Travel_Tower.jpg'
+    }
+}
