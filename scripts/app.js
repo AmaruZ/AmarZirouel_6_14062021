1f565fee8b45954cd0395912c2e21a54c9d712b9
Amar <a.zirouel@gmail.com>
2022-05-14 15:44:52 +0200

    Refactor app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,11 @@
 import { fetchPhotographersJSON } from './getData.js'
 
 const wrapperPhotographers = document.querySelector('.wrapper')
-const spanTags = document.getElementsByClassName('navigation__link')
+const spanTags = document.querySelectorAll('.navigation__link')
+
 export let photographers = []
 
-fetchPhotographersJSON('../data/FishEyeData.json')
+fetchPhotographersJSON('./data/FishEyeData.json')
     .then((data) => {
         data.photographers.forEach((photographer) => {
             photographers.push(photographer)
@@ -12,7 +13,7 @@ fetchPhotographersJSON('../data/FishEyeData.json')
         })
     })
     .then(() => {
-        Array.from(spanTags, (tag) => {
+        spanTags.forEach((tag) => {
             tag.addEventListener('click', () => {
                 filterByTag(tag)
             })
@@ -42,11 +43,14 @@ const addPhotographerInDOM = (photographer) => {
     wrapperPhotographers.appendChild(divPhotographer)
     // prettier-ignore
     divPhotographer.innerHTML = `<a class="photographer__link" href="./pages/photographers.html?id=${photographer.id}">
-                                <img class="photographer__avatar" src="assets/Photographers ID Photos/${photographer.name.replace(' ', '').replace('-', '')}.jpg" alt="${photographer.name}">
-                                <h2 class="photographer__name">${photographer.name}</h2></a>
-                                <p class="photographer__text" tabindex="0"><span class="photographer__localisation">${photographer.city}, ${photographer.country}</span>
-                                <span class="photographer__tagline">${photographer.tagline}</span>
-                                <span class="photographer__price">${photographer.price}€/jour</span></p>
+                                    <img class="photographer__avatar" src="assets/Photographers ID Photos/${photographer.name.replace(' ', '').replace('-', '')}.jpg" alt="${photographer.name}">
+                                    <h2 class="photographer__name">${photographer.name}</h2>
+                                </a>
+                                <p class="photographer__text" tabindex="0">
+                                    <span class="photographer__localisation">${photographer.city}, ${photographer.country}</span>
+                                    <span class="photographer__tagline">${photographer.tagline}</span>
+                                    <span class="photographer__price">${photographer.price}€/jour</span>
+                                </p>
                                 <div class="photographer__tags">${tagInList(photographer.tags)}</div>`
 }
 
@@ -54,45 +58,30 @@ const filterByTag = (tag) => {
     flushPhotographersInDOM()
     if (tag.classList.contains('tags-active')) {
         tag.classList.toggle('tags-active')
-        tag.setAttribute('aria-label', `tag ${tag.textContent.slice(1)} désélectionné`)
-        tag.setAttribute('aria-label', `tag ${tag.textContent.slice(1)}`)
-        tag.removeAttribute('aria-selected')
+        tag.setAttribute('aria-selected', 'false')
         tag.blur()
         photographers.forEach((photographer) => {
             addPhotographerInDOM(photographer)
         })
     } else {
-        for (let i = spanTags.length; i > 0; i--) {
-            if (spanTags[i - 1].classList.contains('tags-active')) {
-                spanTags[i - 1].classList.toggle('tags-active')
-                spanTags[i - 1].setAttribute(
-                    'aria-label',
-                    `tag ${spanTags[i - 1].textContent.slice(1)}`
-                )
-                spanTags[i - 1].removeAttribute('aria-selected')
-                break
+        spanTags.forEach((spanTag) => {
+            if (spanTag.classList.contains('tags-active')) {
+                spanTag.classList.toggle('tags-active')
+                spanTag.setAttribute('aria-label', `tag ${spanTag.textContent.slice(1)}`)
+                spanTag.setAttribute('aria-selected', 'false')
             }
-        }
-
+        })
         tag.classList.toggle('tags-active')
-        tag.setAttribute('aria-label', `tag ${tag.textContent.slice(1)} sélectionné`)
         tag.setAttribute('aria-selected', 'true')
-        for (let i = 0; i < photographers.length; i++) {
-            for (let j = photographers[i].tags.length; j > 0; j--) {
-                console.log(photographers[i])
+        photographers.forEach((photographer) => {
+            photographer.tags.forEach((photographerTag) => {
                 if (
-                    '#' +
-                        photographers[i].tags[j - 1].charAt(0).toUpperCase() +
-                        photographers[i].tags[j - 1].slice(1) ===
+                    '#' + photographerTag.charAt(0).toUpperCase() + photographerTag.slice(1) ===
                     tag.textContent
-                ) {
-                    addPhotographerInDOM(photographers[i])
-                    break
-                } else {
-                    console.log('la')
-                }
-            }
-        }
+                )
+                    addPhotographerInDOM(photographer)
+            })
+        })
     }
 }
 
